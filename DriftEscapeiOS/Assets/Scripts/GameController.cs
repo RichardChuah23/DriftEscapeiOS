3b2b6f9014037686d5a6f94657793d590e9602fa
richardchuah23 <rcchuah@gmail.com>
2018-03-25 13:34:00 +1100

    Added Menu

---
+++ DriftEscapeiOS/Assets/Scripts/GameController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine; 
+
+public class GameController : MonoBehaviour {
+
+    public static GameController instance; 
+    public Button btnStart; 
+    private bool startGame; 
+
+
+	// Use this for initialization
+	void Awake () {
+
+
+        if (instance = this){
+            instance = this; 
+
+            startGame = false; 
+            btnStart.onClick.AddListener(StartClicked);
+        
+        }else if (instance != this){
+
+            Destroy(gameObject);
+        }
+
+
+	}
+
+
+    void StartClicked(){
+
+        startGame = true; 
+        btnStart.gameObject.SetActive(false);
+        Debug.Log("Done ");
+
+        //Do Something
+    
+    }
+
+	// Update is called once per frame
+	void Update () {
+        
+
+	}
+}
