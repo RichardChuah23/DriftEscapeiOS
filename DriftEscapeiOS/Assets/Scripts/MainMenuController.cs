cd12c7aa1fb4991cf538e8954c7cead56b942a75
PeilingCHAN <peilingchan1@gmail.com>
2018-05-02 23:37:30 +1000

    make the car clickable

--- DriftEscapeiOS/Assets/Scripts/MainMenuController.cs
+++ DriftEscapeiOS/Assets/Scripts/MainMenuController.cs
@@ -6,13 +6,58 @@ using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour {
 
+	void Update() {
+		if(Input.GetMouseButtonDown(0)){
+
+			Debug.Log ("Mouse is down");
+
+			RaycastHit hitInfo = new RaycastHit();
+			bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo);
+			Debug.Log("Hit " + hitInfo.transform.gameObject.name);
+			if (hit) {
+				Debug.Log("Hit " + hitInfo.transform.gameObject.name);
+				if (hitInfo.transform.gameObject.tag == "Player") {
+					Debug.Log ("It's working!");
+				} else {
+					Debug.Log ("It's NOT working!");
+				}
+			} else {
+				Debug.Log("No hit");
+			}
+		}
+	}
+
+
+	/*void Start() {
+		var player = GameObject.FindWithTag ("Player").transform;
+		Debug.Log ("This is the " + player);
+	}*/
+	/*void Update() {
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hit;
+
+		if(Physics.Raycast (ray, out hit)) {
+			if (hit.transform.CompareTag("Player")) 
+			//if(hit.transform.gameObject.tag == "Player")
+			{
+				Debug.Log ("This is a Player");
+			}
+			else {
+				Debug.Log ("This isn't a Player");                
+			}
+		}
+	}*/
+
 	/*void Start() {
 		if ( GameObject.Find ("AE86") != null) {
 			Debug.Log ("Exist");
-			GameObject.Find("AE86").
 		} else {
 			Debug.Log ("Not exist");
 		}
+	}*/
+
+	/*void OnMouseDown() {
+		Debug.Log (gameObject.name);
 	}*/
 
     public void ChangeScene(int changeTheScene){
