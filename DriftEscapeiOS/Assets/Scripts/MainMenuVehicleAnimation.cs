c37486ac5a12f7de025f05e23ef958ae10e76556
richardchuah23 <rcchuah@gmail.com>
2018-06-18 18:03:27 +1000

    Scroll View, Car prefab

---
+++ DriftEscapeiOS/Assets/Scripts/MainMenuVehicleAnimation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuVehicleAnimation : MonoBehaviour {
+
+    public Animator anim; 
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		
+	}
+
+    public void playStartEngine(){
+        anim.Play("StartEngine");
+    }
+}
